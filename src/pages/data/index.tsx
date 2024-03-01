e661d1f2718c58144608bb5ea6f3ddf25233e4d5
Hussain Ali <HussainAli22422@gmail.com>
2024-03-01 02:07:14 -0800

    color coding

--- src/pages/data/index.tsx
+++ src/pages/data/index.tsx
@@ -11,6 +11,7 @@ const DataPage = () => {
   const [selectedInstitution, setSelectedInstitution] = useState('');
   const [residueNumber, setResidueNumber] = useState('');
   const [characterizationData, setCharacterizationData] = useState<any[]>([]); // This holds all the rows in the CharacterizationData table in the BglB database
+  const [WTValues, setWTValues] = useState<any>(null);
 
   // Placeholder for the number of records found
   const recordsFound = 222; // Placeholder value
@@ -26,6 +27,26 @@ const DataPage = () => {
       const response = await fetch('/api/getCharacterizationData');
       const data = await response.json();
       setCharacterizationData(data);
+
+      // For color coding 
+      const WT_row = data.find((row:any) => row.id === 1); 
+      if (WT_row) {
+        const WT_log_inv_KM = Math.log10(1 / WT_row.KM_avg);
+        const WT_log_kcat = Math.log10(WT_row.kcat_avg);
+        const WT_log_kcat_over_KM = Math.log10(WT_row.kcat_over_KM);
+        const WT_T50 = WT_row.T50;
+        const WT_Tm = WT_row.Tm;
+        const WT_Rosetta_score = WT_row.Rosetta_score;
+  
+        setWTValues({
+          WT_log_inv_KM,
+          WT_log_kcat,
+          WT_log_kcat_over_KM,
+          WT_T50,
+          WT_Tm,
+          WT_Rosetta_score
+        });
+      }
     };
     const fetchSequences = async () => {
       const response = await fetch('/api/getSequenceData');
@@ -63,10 +84,8 @@ const DataPage = () => {
         return 'WT';
       }
         
-      // Find the corresponding sequence data entry
+      // You can't just subtract 3 from the rosetta num to get the PBD num. You have to perform the lookup on the Sequence table
       const sequenceEntry = sequences.find(seq => seq.resid === resid && seq.Rosetta_resnum === parseInt(resnum, 10));
-        
-      // Determine the correct resnum based on the checkbox state
       const correctResnum = useRosettaNumbering ? sequenceEntry?.Rosetta_resnum : sequenceEntry?.PDBresnum || resnum;
         
       const variant = `${resid}${correctResnum}${resmut}`;
@@ -99,13 +118,15 @@ const DataPage = () => {
         groupedData[key].push(data);
       });
     
+      // NOTE: we are mutating the original data. So if you want to access NON NUMERICAL COLUMNS from here on out (like expressed, which is a boolean), define them here or it won't work 
       displayData = Object.values(groupedData).map((group: any) => {
         const averageRow: any = {
           resid: group[0].resid,
           resnum: group[0].resnum,
           resmut: group[0].resmut,
           isAggregate: group.length > 1,
-          count: group.length
+          count: group.length, 
+          expressed: group.some((item: any) => item.expressed)
         };
       
         const sums: any = {};
@@ -133,6 +154,22 @@ const DataPage = () => {
         return averageRow;
       });
     }
+
+    const getColorForValue = (value:any) => {
+      if (value < -4.75) return '#36929A';
+      else if (value < -4.25) return '#4A9DA4';
+      else if (value < -3.75) return '#5EA8AE';
+      else if (value < -3.25) return '#72B2B8';
+      else if (value < -2.75) return '#86BDC2';
+      else if (value < -2.25) return '#9AC8CC';
+      else if (value < -1.75) return '#AAD3D6';
+      else if (value < -1.25) return '#C2DEE0';
+      else if (value < -0.75) return '#D7E9EB';
+      else if (value < -0.25) return '#EBF4F5';
+      else if (value > 0.25 && value <= 0.75) return '#FAC498';
+      else if (value > 0.75) return '#F68932';
+      else return '#FFFFFF'; 
+    };
 
   return (
     <div className="flex flex-col items-center p-4">
@@ -218,15 +255,51 @@ const DataPage = () => {
           </thead>
           <tbody>
             {displayData.map((data, index) => (
+              
               <tr key={index}>
-              <td className="border border-gray-300">{data.isAggregate ? (<span title={`Average of ${data.count} separate experiments. Click to expand`} className="text-blue-500" onClick={() => setExpandData(true)} style={{cursor: 'pointer'}}>►</span>) : ''}{getVariantDisplay(data.resid, data.resnum, data.resmut)}</td>
-              <td className="border border-gray-300">{data.yield_avg !== null && !isNaN(data.yield_avg) ? roundTo(data.yield_avg, 2) : '—'}</td>
-              <td className="border border-gray-300">{data.KM_avg !== null && !isNaN(data.KM_avg) ? `${roundTo(data.KM_avg, 2)} ± ${data.KM_SD !== null && !isNaN(data.KM_SD) ? roundTo(data.KM_SD, 2) : '—'}` : '—'}</td>
-              <td className="border border-gray-300">{data.kcat_avg !== null && !isNaN(data.kcat_avg) ? `${roundTo(data.kcat_avg, 1)} ± ${data.kcat_SD !== null && !isNaN(data.kcat_SD) ? roundTo(data.kcat_SD, 1) : '—'}` : '—'}</td>
-              <td className="border border-gray-300">{data.kcat_over_KM !== null && !isNaN(data.kcat_over_KM) ? `${roundTo(data.kcat_over_KM, 2)} ± ${data.kcat_over_KM_SD !== null && !isNaN(data.kcat_over_KM_SD) ? roundTo(data.kcat_over_KM_SD, 2) : '—'}` : '—'}</td>
-              <td className="border border-gray-300">{data.T50 !== null && !isNaN(data.T50) ? `${roundTo(data.T50, 1)} ± ${data.T50_SD !== null && !isNaN(data.T50_SD) ? roundTo(data.T50_SD, 1) : '—'}` : '—'}</td>
-              <td className="border border-gray-300">{data.Tm !== null && !isNaN(data.Tm) ? `${roundTo(data.Tm, 1)} ± ${data.Tm_SD !== null && !isNaN(data.Tm_SD) ? roundTo(data.Tm_SD, 1) : '—'}` : '—'}</td>
-              <td className="border border-gray-300">{data.Rosetta_score !== null && !isNaN(data.Rosetta_score) ? roundTo(data.Rosetta_score, 1) : '—'}</td>
+              {/* Variant cells */}
+              <td className="border border-gray-300">
+                {data.isAggregate ? (
+                  <span title={`Average of ${data.count} separate experiments. Click to expand`} className="text-blue-500" onClick={() => setExpandData(true)} style={{cursor: 'pointer'}}>►</span>
+                ) : ''}
+                {getVariantDisplay(data.resid, data.resnum, data.resmut)}
+              </td>
+              
+              {/* Yield cells */}
+              <td className="border border-gray-300" style={{ backgroundColor: data.expressed ? '#000000' : '#FFFFFF', color: data.expressed ? '#FFFFFF' : '#000000'}}>
+              {data.yield_avg !== null && !isNaN(data.yield_avg) ? roundTo(data.yield_avg, 2) : data.expressed ? '*' : '—'}
+              </td>
+
+              {/* KM_avg cells */}
+              <td className="border border-gray-300" style={{backgroundColor: getColorForValue(data.KM_avg !== null && !isNaN(data.KM_avg) ? Math.log10(1 / data.KM_avg) - WTValues.WT_log_inv_KM : -5)}}>
+                {data.KM_avg !== null && !isNaN(data.KM_avg) ? `${roundTo(data.KM_avg, 2)} ± ${data.KM_SD !== null && !isNaN(data.KM_SD) ? roundTo(data.KM_SD, 2) : '—'}` : '—'}
+              </td>
+
+              {/* kcat_avg cells */}
+              <td className="border border-gray-300" style={{backgroundColor: getColorForValue(data.kcat_avg !== null && !isNaN(data.kcat_avg) ? Math.log10(data.kcat_avg) - WTValues.WT_log_kcat : -5)}}>
+                {data.kcat_avg !== null && !isNaN(data.kcat_avg) ? `${roundTo(data.kcat_avg, 1)} ± ${data.kcat_SD !== null && !isNaN(data.kcat_SD) ? roundTo(data.kcat_SD, 1) : '—'}` : '—'}
+              </td>
+
+              {/* kcat_over_KM cells */}
+              <td className="border border-gray-300" style={{backgroundColor: getColorForValue(data.kcat_over_KM !== null && !isNaN(data.kcat_over_KM) ? Math.log10(data.kcat_over_KM) - WTValues.WT_log_kcat_over_KM : -5)}}>
+                {data.kcat_over_KM !== null && !isNaN(data.kcat_over_KM) ? `${roundTo(data.kcat_over_KM, 2)} ± ${data.kcat_over_KM_SD !== null && !isNaN(data.kcat_over_KM_SD) ? roundTo(data.kcat_over_KM_SD, 2) : '—'}` : '—'}
+              </td>
+              
+              {/* T50 cells */}
+              <td className="border border-gray-300" style={{backgroundColor: getColorForValue(data.T50 !== null && !isNaN(data.T50) ? data.T50 - WTValues.WT_T50 : -5)}}>
+                {data.T50 !== null && !isNaN(data.T50) ? `${roundTo(data.T50, 1)} ± ${data.T50_SD !== null && !isNaN(data.T50_SD) ? roundTo(data.T50_SD, 1) : '—'}` : '—'}
+              </td>
+              
+              {/* Tm cells */}
+              <td className="border border-gray-300" style={{backgroundColor: getColorForValue(data.Tm !== null && !isNaN(data.Tm) ? data.Tm - WTValues.WT_Tm : -5)}}>
+                {data.Tm !== null && !isNaN(data.Tm) ? `${roundTo(data.Tm, 1)} ± ${data.Tm_SD !== null && !isNaN(data.Tm_SD) ? roundTo(data.Tm_SD, 1) : '—'}` : '—'}
+              </td>
+
+              {/* Rosetta cells */}
+              <td className="border border-gray-300" style={{backgroundColor: getColorForValue(data.Rosetta_score !== null && !isNaN(data.Rosetta_score) ? -(data.Rosetta_score - WTValues.WT_Rosetta_score) : 0)}}>
+                {data.Rosetta_score !== null && !isNaN(data.Rosetta_score) ? roundTo(data.Rosetta_score, 1) : '—'}
+              </td>
+
             </tr>
             ))}
           </tbody>
