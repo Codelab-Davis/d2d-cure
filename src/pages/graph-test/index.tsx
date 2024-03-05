1ae38ccd43e38e63dfb2ef9030062998c5ae8e16
rahulb1218 <rbudhiraja@ucdavis.edu>
2024-03-05 13:34:57 -0800

    graph generation using plot_data python script
    very rudimentary graph generation

--- src/pages/graph-test/index.tsx
+++ src/pages/graph-test/index.tsx
@@ -1,11 +1,40 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
 function GraphImage() {
-  // URL of your Flask route
-  const flaskImageUrl = 'http://127.0.0.1:5000/plot';
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleSubmit = async () => {
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/plotit', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        responseType: 'blob',
+        
+      });
+      const imageBlob = new Blob([response.data], { type: 'image/png' });
+      const imageUrl = URL.createObjectURL(imageBlob);
+      console.log(imageUrl); 
+      document.getElementById('graphImage').src = imageUrl;
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  };
 
   return (
     <div>
       <h2>test graph</h2>
-      <img src={flaskImageUrl}/>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleSubmit}>Upload and Plot</button>
+      <img id="graphImage" alt="Graph will be displayed here after uploading."/>
     </div>
   );
 }
