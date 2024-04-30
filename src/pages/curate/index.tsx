e34729b0496d74447e51fb36a3e10338489860a0
Vishal Koppuru <vishalksai@gmail.com>
2024-04-30 12:07:18 -0700

    added selection functionality and fixed sorting, accept/reject not done

--- src/pages/curate/index.tsx
+++ src/pages/curate/index.tsx
@@ -10,31 +10,41 @@ const CuratePage = () => {
 
     const [data, setData] = useState<any[]>([]);
     const [sortColumn, setSortColumn] = useState<string>('ID');
-    const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc');
+    const [sortDirection, setSortDirection] = useState<string>('asc');
+    const [checkedItems, setCheckedItems] = useState<Record<string, boolean>>({});
 
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch('/api/getPendingData');
             const data = await response.json();
-            const sortedData = sortData(data)
+            if (!user) return;
+            const filteredData = data.filter((item:any) => item.institution === user.institution)
+            const sortedData = sortData(filteredData, sortColumn, sortDirection)
             setData(sortedData);
-
+            const initialCheckedItems = data.reduce((state:any, item:any) => ({
+                ...state,
+                [item.id]: false  // Initialize all checkboxes as unchecked
+            }), {} as Record<string, boolean>);
+            setCheckedItems(initialCheckedItems);
         };
         fetchData();
-    }, [])
+    }, [user])
 
     const handleColumnClick = (columnName: string) => {
+        let newSortDirection;
         if (sortColumn === columnName) {
-            setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+            newSortDirection = sortDirection === 'asc' ? 'desc' : 'asc'
+            setSortDirection(newSortDirection);
         } else {
+            newSortDirection = 'asc'
             setSortColumn(columnName);
             setSortDirection('asc');
         }
-        const sortedData = sortData(data)
+        const sortedData = sortData(data, columnName, newSortDirection)
         setData(sortedData);
     };
 
-    function sortData(data: any) {
+    function sortData(data: any, sortColumn: string, sortDirection: string) {
         const sortedData = data.sort((a: any, b: any) => {
             let valA, valB;
             let compareVal;
@@ -46,10 +56,14 @@ const CuratePage = () => {
                 valA = a.resnum;
                 valB = b.resnum;
                 compareVal = valA - valB;
+                if (compareVal === 0) {
+                    compareVal = a.resmut.localeCompare(b.resmut);
+                }
             } else if (sortColumn === 'Creator') {
                 valA = a.creator;
                 valB = b.creator;
-                compareVal = valA.localeComapre(valB);
+                console.log(valA, valB)
+                compareVal = valA.localeCompare(valB);
             }
 
             if (compareVal !== 0) {
@@ -76,18 +90,32 @@ const CuratePage = () => {
         return `${resid}${resnum}${resmut}`;
     };
 
+    const handleCheckboxChange = (id: string) => {
+        setCheckedItems(prevState => ({
+            ...prevState,
+            [id]: !prevState[id]
+        }));
+    };
+
     return (
         <div>
             <NavBar />
             <AuthChecker minimumStatus={"professor"}>
                 <div>
                     <div className="flex flex-col items-center p-4">
+                        <h1 className="text-2xl font-bold">Bulk Curation of Data</h1>
+                        <h2 className="text-xl">Data from the {user?.user_name} Lab or other labs at {user?.institution}</h2>
+
                         <table className="table-auto border-collapse border border-gray-400">
                             <thead>
                                 <tr>
-                                    <th className="border border-gray-300 cursor-pointer" onClick={() => handleColumnClick("ID")}>ID</th>
-                                    <th className="border border-gray-300 cursor-pointer" onClick={() => handleColumnClick("Variant")}>Variant</th>
-                                    <th className="border border-gray-300 cursor-pointer" onClick={() => handleColumnClick("Creator")}>Creator</th>
+                                    <th className="border border-gray-300"/>
+                                    <th className={`border border-gray-300 cursor-pointer ${sortColumn === "ID" ? "text-orange-500" : "text-blue-500"}`}
+                                        onClick={() => handleColumnClick("ID")}>ID {sortColumn === "ID" ? (sortDirection === "asc" ? "▲" : "▼") : ""}</th>
+                                    <th className={`border border-gray-300 cursor-pointer ${sortColumn === "Variant" ? "text-orange-500" : "text-blue-500"}`}
+                                        onClick={() => handleColumnClick("Variant")}>Variant {sortColumn === "Variant" ? (sortDirection === "asc" ? "▲" : "▼") : ""}</th>
+                                    <th className={`border border-gray-300 cursor-pointer ${sortColumn === "Creator" ? "text-orange-500" : "text-blue-500"}`}
+                                        onClick={() => handleColumnClick("Creator")}>Creator {sortColumn === "Creator" ? (sortDirection === "asc" ? "▲" : "▼") : ""}</th>
                                     <th className="border border-gray-300">Km</th>
                                     <th className="border border-gray-300">Kcat</th>
                                     <th className="border border-gray-300">T50</th>
@@ -96,6 +124,13 @@ const CuratePage = () => {
                             <tbody>
                                 {data.map((data, index) => (
                                     <tr key={index}>
+                                        <td className="border border-gray-300">
+                                        <input
+                                            type="checkbox"
+                                            checked={checkedItems[data.id] || false}
+                                            onChange={() => handleCheckboxChange(data.id)}
+                                        />
+                                        </td>
                                         <td className="border border-gray-300">
                                             {data.id}
                                         </td>
